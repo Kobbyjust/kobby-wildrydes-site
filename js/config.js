f44017de14e42982654bb0e219f8d41d7396ab2b
Kobbyjust <sagarinokoe1@gmail.com>
2023-03-14 15:04:59 +0000

    first commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_bGSn8WfRP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7rk74oijqak439sersdnjitp20', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://bbcx1jx3hb.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
